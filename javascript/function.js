9b74284f17e6d600f16598e6e5c23174e551952e
WmmyS <wesleymarcosm@gmail.com>
2020-03-13 13:15:28 -0300

    Criada a primeira interface da calculadora

---
+++ javascript/function.js
@@ -0,0 +1,9 @@
+function digitar(num)
+		{
+			var atual = visor.value;
+			document.getElementById('visor').value = atual + num;
+		}
+	function zerar()
+		{
+			document.getElementById('visor').value = " ";
+		}
