eed897665e297516850bb6910bcfde5114f1c089
WmmyS <wesleymarcosm@gmail.com>
2022-03-05 23:34:02 -0300

    refactor: Ajustes nos métodos de mudança de skin.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,37 +4,79 @@
  * Autor: Wesley Santos
  */
 
+/**
+ * Calculadora da página.
+ */
+const calculadora = {
 
+	display: document.querySelector('#visor'),
+	skin: document.getElementById('calculadora').classList,
+	background: document.getElementById('container'),
+	
+	iniciar() {
+		alert('Calculadora Iniciada')
+	},
 
-/*Função que altera as classe dos elementos*/
-function mudarClasse(elemento, classeNova){
-	elemento.classList.remove('white', 'black', 'red', 'green', 'orange');
-    elemento.classList.add(classeNova);
-}
-function changeSkinWhite(){
-	calculadora = document.getElementById('calculadora')
-	mudarClasse(calculadora, 'white')
-	document.getElementById('container').style.backgroundImage = 'linear-gradient(120deg, gray, white, gray)'
-	document.getElementById('container').style.transition = 0.5;
-}
-function changeSkinBlack(){
-	calculadora = document.getElementById('calculadora')
-	mudarClasse(calculadora, 'black')
-	document.getElementById('container').style.backgroundImage = 'linear-gradient(120deg, black, rgb(80, 80, 80), black)'
-}
-function changeSkinRGB(){
-	calculadora = document.getElementById('calculadora')
-	mudarClasse(calculadora, 'red')
-	document.getElementById('container').style.backgroundImage = 'linear-gradient(120deg, black, rgb(128, 0, 0), black)'
-}
-function changeSkinGreen(){
-	calculadora = document.getElementById('calculadora')
-	mudarClasse(calculadora, 'green')
-	document.getElementById('container').style.backgroundImage = 'linear-gradient(120deg, black, rgb(0, 128, 0), black)'
-}
-function changeSkinOrange(){
-	calculadora = document.getElementById('calculadora')
-	mudarClasse(calculadora, 'orange')
-	document.getElementById('container').style.backgroundImage = 'linear-gradient(120deg, black, rgb(220, 100, 0), black)'
+	cliqueBotoes() {
+		document.addEventListener('click', function (evento) {
+			const elemento = evento.target;
+			if (elemento.classList('number')) {
+				console.log('deu certo')
+			}
+		})
+	},
+
+	mudarSkin(valor){
+		this.skin.forEach(data => this.skin.remove(data));
+		this.skin.add(valor);		
+		mudafundo(valor, this.background);
+	}
 }
 
+calculadora.iniciar();
+
+/**
+ * Altera a cor de skin da calculadora de acordo com o botão escolhido.
+ * @param {String} valor 
+ */
+function changeSkin(valor){
+	calculadora.mudarSkin(valor);
+}
+
+/**
+ * Define u plano de fundo pré-definido à página de acordo com o tema de skin escolhido.
+ * @param {String} key 
+ */
+function mudafundo(key, background) {
+	switch (key) {
+		case 'white':
+			background.style
+				.backgroundImage = 'linear-gradient(120deg, gray, white, gray)';
+			break;
+
+		case 'black':
+			background.style
+				.backgroundImage = 'linear-gradient(120deg, black, rgb(80, 80, 80), black)';
+			break;
+
+		case 'red':
+			background.style
+				.backgroundImage = 'linear-gradient(120deg, black, rgb(128, 0, 0), black)';
+			break;
+
+		case 'green':
+			background.style
+				.backgroundImage = 'linear-gradient(120deg, black, rgb(0, 128, 0), black)';
+			break;
+
+		case 'orange':
+			background.style
+				.backgroundImage = 'linear-gradient(120deg, black, rgb(220, 100, 0), black)';
+			break;
+	
+		default:
+			background.style
+				.backgroundImage = 'linear-gradient(120deg, gray, white, gray)';
+			break;
+	}
+}
